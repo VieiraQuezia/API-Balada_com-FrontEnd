dcb16cf121759f8df855f363a2e49fb74ab29108
Quezia Vieira <vieiraquezia24@gmail.com>
2025-09-17 14:55:15 -0300

    primeiro commit

---
+++ BackEnd/routes/baladaRoutes.js
@@ -0,0 +1,19 @@
+//clienteRoutes.js 
+const express = require('express'); 
+const router = express.Router(); 
+const baladaController = require('../controllers/baladaController'); 
+//Lembrando que a rota raiz tem a palavra clientes, definido no app.js 
+
+// Rota para obter todos as baladas 
+router.get('/', baladaController.getAllBaladas); 
+// Rota para obter uma balada pela cidade 
+router.get('/:cidade', baladaController.getBaladaByCidade); 
+// Rota para obter uma balada pela data 
+router.get('/data/:data', baladaController.getBaladaByData);
+// Rota para criar um novo cliente 
+router.post('/', baladaController.createBalada); 
+// Rota para atualizar um cliente existente 
+router.put('/:id', baladaController.updateBalada); 
+// Rota para deletar um cliente 
+router.delete('/:id', baladaController.deleteBalada); 
+module.exports = router;
